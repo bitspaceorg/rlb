c448a7ae94d1f4c487e9f7d6ed4ad4c95b9f0a36
Surya Prakash <surya0prakash@proton.me>
2024-11-29 13:22:46 +0530

    [Fix] Rename Image Class

--- include/image.hpp
+++ include/image.hpp
@@ -3,12 +3,13 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/photo.hpp>
 
-class CustImage{
+class CustImage {
 private:
 	cv::Mat image;
 public:
 	CustImage(cv::Mat);
 	void display_image();
+
   // returns a image
   void denoise_image();
   void get_gray_image();
