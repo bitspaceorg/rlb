f226d6a97eb7c9b558e73b03b56b9a251467a406
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-12-05 13:01:43 +0530

    update dump

--- include/raylibwrapper.hpp
+++ include/raylibwrapper.hpp
@@ -9,13 +9,13 @@ class RaylibWrapper {
 private:
   Camera3D camera;
   int camera_mode;
-  Color colors[5] = {DARKGRAY, PINK, ORANGE, DARKBLUE, GREEN};
   int window_width;
   int window_height;
   std::string window_title;
 
   float distance(float x1, float y1, float x2, float y2);
   float angle_between_points(float p1x, float p1y, float p2x, float p2y);
+  float cross(const cv::Point2d &O, const cv::Point2d &A, const cv::Point2d &B);
 
 public:
   RaylibWrapper(int width, int height, const std::string &title);
@@ -23,5 +23,13 @@ public:
 
   void init();
   void update_camera();
-  void render(const std::vector<std::vector<cv::Point2d>> &contours);
+  Color colors[6] = {DARKGRAY, PINK, ORANGE, DARKBLUE, GREEN, GRAY};
+  Camera3D get_camera() { return camera; };
+  void render(const std::vector<std::vector<cv::Point2d>> &contours, Color color);
+  void render_base(const std::vector<cv::Point2d> &contours, float z, Color color);
+  void render_base_lines(const std::vector<cv::Point2d> &contours, float z, Color color);
+  std::vector<cv::Point2d> get_convex_hull(std::vector<cv::Point2d> &points);
+  std::vector<cv::Point2d> get_boundary(std::vector<cv::Point2d> &points);
+  std::vector<cv::Point2d> get_bounding_box(std::vector<cv::Point2d> &points);
+  std::vector<cv::Point2d> get_closed_polygon(std::vector<cv::Point2d> &points);
 };
--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include "raylibwrapper.hpp"
 #include "io.hpp"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include "GLFW/glfw3.h"
-#include <iostream>
 
 using namespace cv;
 
 int main() {
-  std::string image_path = "/Users/suryaprakash/Developer/sih/test.jpeg";
-  IOHelper *io = new IOHelper();
-  auto img = io->read_image(image_path);
-  std::vector<std::vector<cv::Point2d>> contours;
-  img.water_shed(contours);
+    std::string image_path = "/Users/rahulnavneeth/projects/rlb-core/test4.jpeg";
+    IOHelper *io = new IOHelper();
+    auto img = io->read_image(image_path);
+    std::vector<std::vector<cv::Point2d>> contours;
+    img.water_shed(contours);
 
-  RaylibWrapper viewer(800, 600, "3D Room Viewer");
-  viewer.init();
+    RaylibWrapper viewer(800, 600, "3D Room Viewer");
+    viewer.init();
 
-  while (!WindowShouldClose()) {
-      viewer.update_camera();
-      viewer.render(contours);
-  }
-};
+    std::vector<cv::Point2d> input_2D;
+    for (const auto &points : contours) {
+        for (const auto &point : points) {
+            input_2D.push_back(point);
+        }
+    }
+
+    std::vector<cv::Point2d> boundary = viewer.get_bounding_box(input_2D);
+
+    Shader aoShader = LoadShader("./src/ambient_occlusion.vs", "ambient_occlusion.fs");
+    SetShaderValue(aoShader, GetShaderLocation(aoShader, "lightDir"), (float[3]){0.0f, 1.0f, 0.0f}, SHADER_UNIFORM_VEC3);
+    SetShaderValue(aoShader, GetShaderLocation(aoShader, "ambientStrength"), (float[1]){0.5f}, SHADER_UNIFORM_FLOAT);
+
+    while (!WindowShouldClose()) {
+        viewer.update_camera();
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(viewer.get_camera());
+        DrawGrid(10, 2.0f);
+
+        BeginShaderMode(aoShader);
+
+        	viewer.render(contours, viewer.colors[5]);
+        	viewer.render_base(boundary, 0.0f, viewer.colors[5]);
+
+        EndShaderMode();
+
+        EndMode3D();
+        EndDrawing();
+    }
+
+    UnloadShader(aoShader);
+    delete io;
+    return 0;
+}
