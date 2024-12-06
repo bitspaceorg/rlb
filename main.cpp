edc857612c9fb7089594b8da56eb06037112e7a9
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-12-06 09:07:33 +0530

    [Fix] Merge resolve

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@
 #define GL_SILENCE_DEPRECATION
 
 int main() {
-  SetConfigFlags(FLAG_MSAA_4X_HINT);  
-  std::string image_path = "/home/naviprem/dev/sih/rlb/test.jpeg";
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+  std::string image_path =
+      "/Users/rahulnavneeth/projects/sih/test_clone/test.jpeg";
   IOHelper *io = new IOHelper();
   auto img = io->read_image(image_path);
   std::vector<std::vector<cv::Point>> contours;
@@ -18,11 +19,13 @@ int main() {
   std::vector<std::vector<cv::Point2d>> ncontours;
   ncontours.resize(contours.size());
 
-  for (int i = 0; i < contours.size(); i ++) {
-      ncontours[i].resize(contours[i].size());
-      for (int j = 0; j < contours[i].size(); j ++) {
-          ncontours[i][j] = contours[i][j];
-      }
+  for (int i = 0; i < contours.size(); i++) {
+    ncontours[i].resize(contours[i].size());
+    for (int j = 0; j < contours[i].size(); j++) {
+      ncontours[i][j] = contours[i][j];
+      std::cout << ncontours[i][j] << "\n";
+    }
+    std::cout << "\n";
   }
 
   RaylibWrapper viewer(800, 600, "3D Room Viewer");
@@ -35,34 +38,42 @@ int main() {
   }
 
   std::vector<cv::Point2d> boundary = viewer.get_bounding_box(input_2D);
-  Shader shader = LoadShader(TextFormat("/Users/suryaprakash/Developer/sih/rlb/shader/lighting.vs", GLSL_VERSION),
-                               TextFormat("/Users/suryaprakash/Developer/sih/rlb/shader/lighting.fs", GLSL_VERSION));
-	shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader,"viewPos");
+  Shader shader = LoadShader(
+      TextFormat(
+          "/Users/rahulnavneeth/projects/sih/test_clone/shader/lighting.vs",
+          GLSL_VERSION),
+      TextFormat(
+          "/Users/rahulnavneeth/projects/sih/test_clone/shader/lighting.fs",
+          GLSL_VERSION));
+  shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
 
-	int ambientLoc = GetShaderLocation(shader,"ambient");
-	SetShaderValue(shader,ambientLoc,(float[4]){1.0f,1.0f,1.0f,1.0f},SHADER_UNIFORM_VEC4);
+  int ambientLoc = GetShaderLocation(shader, "ambient");
+  SetShaderValue(shader, ambientLoc, (float[4]){1.0f, 1.0f, 1.0f, 1.0f},
+                 SHADER_UNIFORM_VEC4);
 
-	Light light = CreateLight(LIGHT_POINT,(Vector3){-1,3,-2},Vector3Zero(),WHITE,shader);
-
+  Light light = CreateLight(LIGHT_POINT, (Vector3){-1, 3, -2}, Vector3Zero(),
+                            WHITE, shader);
 
   while (!WindowShouldClose()) {
     viewer.update_camera();
-    float cameraPos[3] = { viewer.camera.position.x, viewer.camera.position.y, viewer.camera.position.z };
-		SetShaderValue(shader,shader.locs[SHADER_LOC_VECTOR_VIEW],cameraPos,SHADER_UNIFORM_VEC3);
+    float cameraPos[3] = {viewer.camera.position.x, viewer.camera.position.y,
+                          viewer.camera.position.z};
+    SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos,
+                   SHADER_UNIFORM_VEC3);
 
     BeginDrawing();
     ClearBackground(BLACK);
 
     BeginMode3D(viewer.get_camera());
-		BeginShaderMode(shader);
-			viewer.render(ncontours, viewer.colors[5]);
-			viewer.render_base(boundary, 0.0f, viewer.colors[5]);
-		EndShaderMode();
+    BeginShaderMode(shader);
+    viewer.render(ncontours, viewer.colors[5]);
+    viewer.render_base(boundary, 0.0f, viewer.colors[5]);
+    EndShaderMode();
     EndMode3D();
     EndDrawing();
   }
-	UnloadShader(shader);
-	CloseWindow();
+  UnloadShader(shader);
+  CloseWindow();
   delete io;
   return 0;
 };
